c55597400978456c60fd7024201825f141ced261
ThatCipher <deniz_malik@outlook.de>
2023-03-09 13:48:24 +0100

    Added Default Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { LoginViewComponent } from './views/login-view/login-view.component';
 import { SpinnerComponent } from './components/ui-elements/spinner/spinner.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NavbarComponent } from './components/ui-elements/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginViewComponent,
     SpinnerComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
